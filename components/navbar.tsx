4e09eeedc6db05dd4a03b355eda0223114a252a9
LAG-4 <aryangupta4feb@gmail.com>
2025-06-14 01:31:57 +0530

    adding supabase credentials

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import NavItems from "./NavItems";
+import { SignInButton, SignedIn, SignedOut,UserButton } from "@clerk/nextjs";
+
+
+
 const Navbar = () => {
     return (
         <>
@@ -11,9 +15,16 @@ const Navbar = () => {
                         <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
                     </div>
                 </Link>
-                <NavItems/>
                 <div className="flex items-center gap-8">
-                    <p>Sign In</p>
+                    <NavItems/>
+                    <SignedOut>
+                        <SignInButton>
+                            <button className="btn-signin">Sign In</button>
+                        </SignInButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
 
                 </div>
             </nav>
